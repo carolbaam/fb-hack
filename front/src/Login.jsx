1e8f8ae14fdda26e9b8b9f7749be03341ee1cbf7
ikana <roderik.ikana@gmail.com>
2018-07-06 20:50:52 -0500

    add login

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -1,94 +1,90 @@
 /* eslint camelcase: 0 */
 import React from 'react';
 
-
 class InitAccount extends React.Component {
-  state={
-    value: 0,
-    phone: '',
-  }
+  state = {
+    countryCode: '',
+    phoneNumber: '',
+    emailAddress: '',
+  };
 
-  componentDidMount() {
-    AccountKit_OnInteractive = function () {
-      AccountKit.init(
-        {
-          appId: '{{179059249606625}}',
-          state: '{{csrf}}',
-          version: '{{v1.1}}',
-          fbAppEventsEnabled: true,
-          redirect: '{{https://safemeet.space/}}',
-        },
-      );
-    };
-  }
+  onChangeValue = (param, value) => {
+    this.setState(() => ({
+      [param]: value,
+    }));
+  };
 
-    // login callback
-    loginCallback = (response) => {
-      if (response.status === 'PARTIALLY_AUTHENTICATED') {
-        const { code } = response;
-        console.log(code);
+  // login callback
+  loginCallback = (response) => {
+    if (response.status === 'PARTIALLY_AUTHENTICATED') {
+      const { code } = response;
+      console.log(code);
 
-        const { state: csrf } = response;
-        console.log(csrf);
-        // Send code to server to exchange for access token
-      } else if (response.status === 'NOT_AUTHENTICATED') {
-        // handle authentication failure
-      } else if (response.status === 'BAD_PARAMS') {
-        // handle bad parameters
-      }
-    };
-      
-    onChangeValue
-
-     onchangeCountryCode=()=>{
-       this.setState({
-         value:
-       })
-     }
+      const { state: csrf } = response;
+      console.log(csrf);
+      // Send code to server to exchange for access token
+    } else if (response.status === 'NOT_AUTHENTICATED') {
+      // handle authentication failure
+    } else if (response.status === 'BAD_PARAMS') {
+      // handle bad parameters
+    }
+  };
 
   // phone form submission handler
-    smsLogin = () => {
-      this.setState({
-        number: '',
-      });
-      const countryCode = document.getElementById('country_code').value;
-      const phoneNumber = document.getElementById('phone_number').value;
-      AccountKit.login(
-        'PHONE',
-        { countryCode, phoneNumber }, // will use default values if not specified
-        loginCallback,
-      );
-    };
+  smsLogin = () => {
+    const { countryCode, phoneNumber } = this.state;
+    AccountKit.login(
+      'PHONE',
+      { countryCode, phoneNumber }, // will use default values if not specified
+      this.loginCallback,
+    );
+  };
 
+  // email form submission handler
+  emailLogin = () => {
+    const { emailAddress } = this.state;
+    AccountKit.login('EMAIL', { emailAddress }, this.loginCallback);
+  };
 
-    // email form submission handler
-    emailLogin = () => {
-      const emailAddress = document.getElementById('email').value;
-      AccountKit.login(
-        'EMAIL',
-        { emailAddress },
-        loginCallback,
-      );
-    };
-
-    render() {
-      return (
+  render() {
+    const { countryCode, phoneNumber, emailAddress } = this.state;
+    return (
+      <div>
+        <input
+          value={countryCode}
+          onChange={(value) => {
+            this.onChangeValue('countryCode', value);
+          }}
+          id="country_code"
+        />
+        <input
+          value={phoneNumber}
+          onChange={(value) => {
+            this.onChangeValue('phoneNumber', value);
+          }}
+          placeholder="phone number"
+          id="phone_number"
+        />
+        <button type="button" onClick={this.smsLogin}>
+          Login via SMS
+        </button>
         <div>
-          <input value={this.state.value} onChange={this.onchangeCountryCode} id="country_code" />
-          <input value={this.state.number} onChange={this.onchangePhoneNumber} placeholder="phone number" id="phone_number" />
-          <button type="button" onClick={this.smsLogin}>
-Login via SMS
-          </button>
-          <div>
-OR
-          </div>
-          <input placeholder="email" id="email" />
-          <button type="button" onClick={this.emailLogin}>
-Login via Email
-          </button>
+        OR
         </div>
-      );
-    }
+        <input
+          value={emailAddress}
+          placeholder="email"
+          id="email"
+          onChange={(value) => {
+            this.onChangeValue('emailAddress', value);
+          }}
+        />
+        <button type="button" onClick={this.emailLogin}>
+          Login via Email
+        </button>
+      </div>
+    );
+  }
 }
 
 export default InitAccount;
